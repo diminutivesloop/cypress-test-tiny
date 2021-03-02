1e7d95b20b65bd19227bb7613853e354207aeba1
Nathaniel Palmer <npalmer@follett.com>
2021-03-02 16:19:09 -0500

    add test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,15 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
+describe('focus', () => {
+  it('should not scroll focused element into view when using preventScroll option', () => {
     cy.visit('https://example.cypress.io')
+
+    cy.scrollTo("bottom");
+
+    cy.get('.navbar-brand')
+      .then((el) => {
+        el.get(0).focus({ preventScroll: true });
+      })
+      .should("be.focused")
+      .should("not.be.visible");
   })
 })
